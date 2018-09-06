ac4785ed5a9db22482dc86c8d0b6dc83949498ed
David Paiva <pillgp@icloud.com>
2018-09-06 02:35:10 +0100

    :thinking:

--- src/structures/MinecraftUtils.js
+++ src/structures/MinecraftUtils.js
@@ -3,7 +3,7 @@ const rp = require('request-promise-native')
 const convertUUID = (user) => {
   return new Promise((resolve) => {
     rp.get(`https://api.mojang.com/users/profiles/minecraft/${user}`).then(res => {
-      const response = JSON.parse(res)
+      const response = res ? JSON.parse(res) : null
       if (!response) resolve('8667ba71b85a4004af54457a9734eed7')
       else resolve(response.id)
     })
