c8fc4d631683d7ba3c9e56730a940c8280302827
David Paiva <pillgp@icloud.com>
2018-09-22 00:01:53 +0100

    Let's add a main router so, yeah.

---
+++ src/routes/main.js
@@ -0,0 +1,25 @@
+const { Route } = require('../index')
+const { Router } = require('express')
+
+module.exports = class Main extends Route {
+  constructor (client) {
+    super(client)
+    this.name = ''
+  }
+
+  load () {
+    const router = Router()
+
+    router.get('/', (req, res) => {
+      res.status(200).json({ endpoints: [
+        'GET /minecraft'
+      ] })
+    })
+
+    router.get('/ping', (req, res) => {
+      res.status(200).json({ message: 'OK' })
+    })
+
+    return router
+  }
+}
