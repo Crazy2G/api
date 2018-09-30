be49c144876fc92494eb89d49ea625eb1a2c1056
David Paiva <pillgp@icloud.com>
2018-10-01 00:08:15 +0100

    Jpeg-ify endpoint (thx Hikari mmLol)

---
+++ src/routes/image.js
@@ -0,0 +1,41 @@
+const { Route } = require('../index')
+const { Router } = require('express')
+const Jimp = require('jimp')
+
+module.exports = class Image extends Route {
+  constructor (client) {
+    super(client)
+    this.name = 'image'
+  }
+
+  load () {
+    const router = Router()
+
+    router.get('/', (req, res) => {
+      res.status(200).json({
+        endpoints: [
+          'GET /ping',
+          'POST /jpeg'
+        ]
+      })
+    })
+
+    router.get('/ping', (req, res) => {
+      res.status(200).json({ message: 'OK' })
+    })
+
+    router.post('/jpeg', async (req, res) => {
+      if (!req.body.image) return res.status(400).json({ message: 'You need to specify a image to jpegify from' })
+      else {
+        if (req.body.quality > 100 || req.body.quality < 0) return res.status(400).json({ message: 'Quality must be a number between 0 and 100' })
+        const imageURL = req.body.image
+        const quality = req.body.quality || 1
+
+        const image = await Jimp.read(imageURL).then(img => img.quality(quality))
+        res.status(200).set('Content-Type', 'image/jpeg').send(await image.getBufferAsync(Jimp.MIME_JPEG))
+      }
+    })
+
+    return router
+  }
+}
