956fc3792319001ba9ce4c7f591da24491a92a79
David Paiva <pillgp@icloud.com>
2018-10-05 12:37:27 +0100

    Added /image/blur endpoint

--- src/routes/image.js
+++ src/routes/image.js
@@ -15,7 +15,8 @@ module.exports = class Image extends Route {
       res.status(200).json({
         endpoints: [
           'GET /ping',
-          'POST /jpeg'
+          'POST /jpeg',
+          'POST /blur'
         ]
       })
     })
@@ -33,6 +34,18 @@ module.exports = class Image extends Route {
 
         const image = await Jimp.read(imageURL).then(img => img.quality(quality))
         res.status(200).set('Content-Type', 'image/jpeg').send(await image.getBufferAsync(Jimp.MIME_JPEG))
+      }
+    })
+
+    router.post('/blur', async (req, res) => {
+      if (!req.body.image) return res.status(400).json({ message: 'You need to specify a image to blur from' })
+      else {
+        if (req.body.intensity && (req.body.intensity > 100 || req.body.intensity < 0)) return res.status(400).json({ message: 'Intensity must be a number between 0 and 100' })
+        const imageURL = req.body.image
+        const intensity = req.body.intensity || 5
+
+        const image = await Jimp.read(imageURL).then(img => img.blur(intensity))
+        res.status(200).set('Content-Type', 'image/png').send(await image.getBufferAsync(Jimp.MIME_PNG))
       }
     })
 
